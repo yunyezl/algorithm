09232ecd71dabf85370da13d94ff298fe4735769
yunyezl <69361613+yunyezl@users.noreply.github.com>
2020-12-27 03:16:06 +0900

    Update 방금그곡.py

--- programmers/방금그곡.py
+++ programmers/방금그곡.py
@@ -1,22 +1,32 @@
+import re
+
 def solution(m, musicinfos):
-    answer = ''
     mi = {}
-    
+
     for i in musicinfos:
-        temp = i.split(",")
-        start = temp[0]
-        end = temp[1]
-        title = temp[2]
-        code = temp[3]
-        playTime = int(end[-2:]) - int(start[-2:])
+        start, end, title, code = i.split(',')
+        code = code.replace('C#', 'c').replace('D#', 'd').replace('F#','f').replace('G#','g').replace('A#','a')
+        start_h, start_m, end_h, end_m = map(int, start.split(':') + end.split(':'))
+        playTime = 60*(end_h-start_h) + (end_m-start_m)
         if playTime > len(code):
             gap = playTime - len(code)
             playLine = code + code*(gap//len(code)) + code[:gap%len(code)]
         elif playTime < len(code):
             playLine = code[:playTime]
         else:
-            playLine = len(code)
-        mi[title] = [playLine, playTime]
-        
-    print(mi)
-    return answer
+            playLine = code
+        mi[title] = [''.join(playLine), playTime]
+
+    candidate = []
+    m = m.replace('C#', 'c').replace('D#', 'd').replace('F#','f').replace('G#','g').replace('A#','a')
+    for title, info in mi.items():
+        info[0] = info[0].replace('C#', 'c').replace('D#', 'd').replace('F#','f').replace('G#','g').replace('A#','a')
+        if m in info[0]:
+            candidate.append([title, mi[title][1]])
+
+    if len(candidate) == 0:
+        return "(None)"
+    elif len(candidate) > 1:
+        candidate.sort(key=lambda x:-x[1])
+
+    return candidate[0][0]
