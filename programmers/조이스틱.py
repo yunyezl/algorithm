4a6274c2953963d856499b4340b25f4a937d4586
yunyezl <69361613+yunyezl@users.noreply.github.com>
2020-12-31 03:34:19 +0900

    Create 조이스틱.py

---
+++ programmers/조이스틱.py
@@ -0,0 +1,18 @@
+def solution(name):
+    alpa_distance = [min(ord(i) - ord("A"), ord("Z") - ord(i)+1) for i in name]
+    idx, answer = 0, 0
+    while True:
+        answer += alpa_distance[idx]
+        alpa_distance[idx] = 0
+        if sum(alpa_distance) == 0:
+            break
+        left, right = 1, 1
+        while alpa_distance[idx - left] == 0:
+            left += 1
+        while alpa_distance[idx + right] == 0:
+            right += 1
+        answer += left if left < right else right
+        idx += -left if left < right else right
+    return answer
+    
+#그리디
