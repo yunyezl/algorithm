91abf218481d50e1e723cdb310c4bfd9a2272fcf
yunyezl <69361613+yunyezl@users.noreply.github.com>
2021-01-15 22:38:50 +0900

    Create 볼링공 고르기.py

---
+++ 이코테/그리디/볼링공 고르기.py
@@ -0,0 +1,32 @@
+# solution 1
+n, m = map(int, input().split())
+bowlingballs = list(map(int, input().split()))
+
+combi = []
+for i in range(len(bowlingballs)):
+    for j in range(i, len(bowlingballs)):
+        if bowlingballs[i] != bowlingballs[j]:
+            combi.append((i+1, j+1))
+
+print(combi)
+
+# ----------------
+
+# solution 2 (그리디 풀이)
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+
+# 1부터 10까지의 무게를 담을 수 있는 리스트
+array = [0] * 11
+
+for x in data:
+    # 각 무게에 해당하는 볼링공의 개수 카운트
+    array[x] += 1
+
+result = 0
+# 1부터 m까지의 각 무게에 대하여 처리
+for i in range(1, m + 1):
+    n -= array[i] # 무게가 i인 볼링공의 개수(A가 선택한 개수 제외, 안 그러면 나중에 중복 발생)
+    result += array[i] * n # B가 선택하는 경우의 수와 곱하기
+
+print(result)
