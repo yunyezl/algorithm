010015f146ca9e87ac0f9594fa36565ebb07a50f
yunyezl <69361613+yunyezl@users.noreply.github.com>
2021-01-15 21:36:51 +0900

    Create 만들 수 없는 금액.py

---
+++ 이코테/그리디/만들 수 없는 금액.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+
+n = int(input())
+coins = list(map(int, input().split()))
+minimum = int(1e9)
+all = []
+
+for i in range(1, len(coins)):
+    all += list(map(sum, combinations(coins, i)))
+
+all.sort()
+for i in range(len(all) - 1):
+    if all[i] + 1 in all:
+        continue
+    else:
+        answer = all[i] + 1
+        break
+
+print(answer)
